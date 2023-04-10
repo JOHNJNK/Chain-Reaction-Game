0e5a171264bdda919faf24d74d77e097b789a2c5
JOHN NESA KUMAR <johnjnk2000@gmail.com>
2023-04-10 15:07:08 +0530

    js code using class

--- gamescript.js
+++ gamescript.js
@@ -1,182 +1,201 @@
-
-
-
-let chance = 1;
-
-reset = () => {
-
-    document.getElementById("grid").innerHTML = "";
-    currentPlayer = 1;
-    grid();
-}
-
-change = (currentPlayer) => {
-    if (currentPlayer == 1) {
-        return "red";
-    }
-    else {
-        return "blue";
-    }
-}
-
-
-check = (currentPlayer, col) => {
-    let color = col.style.backgroundColor;
-    if (color === "white") {
-        return true;
-    }
-    else if (currentPlayer == 1 && color == "red") {
-        return true;
-    }
-    else if (currentPlayer == 2 && color == "blue") {
-        return true;
-    }
-    return false;
-}
-
-checkForResult = (table) => {
-    let playerOne = 0;
-    let playerTwo = 0;
-    for (let row of table.rows) {
-        for (let col of row.cells) {
-            let color = col.style.backgroundColor;
-            if (color == "red") {
-                playerOne++;
-            }
-            else if (color == "blue") {
-                playerTwo++;
-            }
-        }
-    }
-    if (playerOne == 0 && chance == 0) {
-        alert("Player Two Wins");
-        chance++;
-        reset();
-    }
-    if (playerTwo == 0 && chance == 0) {
-        alert("Player One Wins");
-        chance++;
-        reset();
-    }
-    if (playerOne == 0 || playerTwo == 0) {
-        chance--;
+class ChainReaction {
+    constructor() {
+        this.chance = 1;
+        this.currentPlayer = 1;
     }
 
-    console.log(playerOne + "  " + playerTwo + " " + chance);
-}
+    reset() {
 
-actionwork = (col, currentPlayer) => {
-    // console.log(currentPlayer);
-    if (currentPlayer == 1) {
-        col.style.backgroundColor = "red";
+        // document.getElementById("grid").textContent = "";
+        const grid=document.getElementById("tableGrid");
+        createBlock.style.display = "block";
+        grid.remove();
+        document.getElementById("player").textContent = "Player One(Red)";
+        console.log("Game Over");
     }
-    if (currentPlayer == 2) {
-        col.style.backgroundColor = "blue";
-    }
-    if (col.innerHTML == "*") {
-        col.innerHTML = 1;
-    }
-    else {
-        let colMaxLength = col.parentNode.cells.length - 1;
-        let rowMaxLength = col.parentNode.parentNode.rows.length - 1;
-        let num = parseInt(col.innerHTML) + 1;
-        let maxNum = 4;
-        if ((col.parentNode.rowIndex === 0 && col.cellIndex === 0) ||
-            (col.parentNode.rowIndex === 0 && col.cellIndex === colMaxLength) ||
-            (col.parentNode.rowIndex === rowMaxLength && col.cellIndex === 0) ||
-            (col.parentNode.rowIndex === rowMaxLength && col.cellIndex === colMaxLength)) {
-            maxNum = 2;
-        }
-        else if (col.parentNode.rowIndex === 0 || col.cellIndex === 0 ||
-            col.parentNode.rowIndex === rowMaxLength || col.cellIndex === colMaxLength) {
-            maxNum = 3;
-        }
-        if (num == maxNum) {
-            col.innerHTML = "*";
-            col.style.backgroundColor = "white";
-            let previousRow = col.parentNode.previousSibling;
-            let nextRow = col.parentNode.nextSibling;
-            if (previousRow) {
-                let previousRowCell = previousRow.children[col.cellIndex];
-                if (previousRowCell) {
-                    previousRowCell.style.backgroundColor = change(currentPlayer);
-                    actionwork(previousRowCell, currentPlayer);
-                }
-            }
-            if (nextRow) {
-                let nextRowCell = nextRow.children[col.cellIndex];
-                if (nextRowCell) {
-                    nextRowCell.style.backgroundColor = change(currentPlayer);
-                    actionwork(nextRowCell, currentPlayer);
-                }
-            }
-            let previousCell = col.previousSibling;
-            let nextCell = col.nextSibling;
-            if (previousCell) {
-                previousCell.style.backgroundColor = change(currentPlayer);
-                actionwork(previousCell, currentPlayer);
-            }
-            if (nextCell) {
-                nextCell.style.backgroundColor = change(currentPlayer);
-                actionwork(nextCell, currentPlayer);
-            }
+
+    change(game) {
+        if (game.currentPlayer == 1) {
+            return "red";
         }
         else {
-            col.innerHTML = num;
+            return "blue";
         }
     }
-}
 
-grid = () => {
-
-    let table = document.getElementById("grid");
-
-    // let rowSize = document.getElementById("rowSize").value;
-    // let colSize = document.getElementById("colSize").value;
-    let currentPlayer = 1;
-    table.style.border = "1px solid black";
-    table.style.width = "50%";
-    table.style.margin = "auto";
-
-    for (let i = 0; i < 10; i++) {
-        let row = document.createElement("tr");
-        for (let j = 0; j < 10; j++) {
-            let col = document.createElement("td");
-            col.style.border = "1px solid black";
-            col.style.margin = "auto";
-            col.innerHTML = "*";
-            col.style.backgroundColor = "white";
-
-            col.addEventListener("click", function () {
+    checkForResult(table, game ) {
+        let playerOne = 0;
+        let playerTwo = 0;
+        for (let row of table.rows) {
+            for (let col of row.cells) {
                 let color = col.style.backgroundColor;
-                let check= false;
-                if (color === "white" || (currentPlayer == 1 && color == "red") || (currentPlayer == 2 && color == "blue") ){
-                    check=true;
+                if (color == "red") {
+                    playerOne++;
                 }
-                if (check) {
-                    actionwork(col, currentPlayer);
-                    if (currentPlayer == 1) {
-                        currentPlayer = 2;
-                        document.getElementById("player").innerHTML = "Player Two(Blue)";
-                    }
-                    else {
-                        currentPlayer = 1;
-                        document.getElementById("player").innerHTML = "Player One(Red)";
-                    }
-                    checkForResult(table);
+                else if (color == "blue") {
+                    playerTwo++;
                 }
-            });
-            row.appendChild(col);
+            }
         }
-        table.appendChild(row);
+        if (playerOne == 0 && game.chance == 0) {
+            alert("Player Two Wins");
+            game.chance++;
+            game.reset();
+        }
+        if (playerTwo == 0 && this.chance == 0) {
+            alert("Player One Wins");
+            game.chance++;
+            game.reset();
+        }
+        if (playerOne == 0 || playerTwo == 0) {
+            game.chance--;
+        }
 
+        console.log(playerOne + "  " + playerTwo + " " + game.chance);
+    }
+
+    actionwork(col,game) {
+        const rows = document.getElementById("tableGrid");
+        if (game.currentPlayer == 1) {
+            col.style.backgroundColor = "red";
+        }
+        else{
+            col.style.backgroundColor = "blue";
+        }
+
+
+        if (col.textContent == "*") {
+            col.textContent = 1;
+        }
+
+        else {
+
+            const cols = document.getElementById("row" + parseInt(col.id));
+            
+            console.log(rows);
+            let colMaxLength = cols.childElementCount - 1;
+            let rowMaxLength = rows.childElementCount - 1;
+
+            let num = parseInt(col.textContent) + 1;
+            let maxNum = 4;
+
+            if ((cols.rowIndex === 0 && col.cellIndex === 0) ||
+                (cols.rowIndex === 0 && col.cellIndex === colMaxLength) ||
+                (cols.rowIndex === rowMaxLength && col.cellIndex === 0) ||
+                (cols.rowIndex === rowMaxLength && col.cellIndex === colMaxLength)) {
+                maxNum = 2;
+            }
+            else if (cols.rowIndex === 0 || col.cellIndex === 0 ||
+                cols.rowIndex === rowMaxLength || col.cellIndex === colMaxLength) {
+                maxNum = 3;
+            }
+            if (num == maxNum) {
+                col.textContent = "*";
+                col.style.backgroundColor = "white";
+
+                let previousRow = cols.previousSibling;
+                let nextRow = cols.nextSibling;
+                if (previousRow) {
+                    let previousRowCell = previousRow.children[col.cellIndex];
+                    if (previousRowCell) {
+                        previousRowCell.style.backgroundColor = game.change(game);
+                        game.actionwork(previousRowCell, game);
+                    }
+                }
+                if (nextRow) {
+                    let nextRowCell = nextRow.children[col.cellIndex];
+                    if (nextRowCell) {
+                        nextRowCell.style.backgroundColor = game.change(game);
+                        game.actionwork(nextRowCell, game);
+                    }
+                }
+                let previousCell = col.previousSibling;
+                let nextCell = col.nextSibling;
+                if (previousCell) {
+                    previousCell.style.backgroundColor = game.change(game);
+                    game.actionwork(previousCell, game);
+                }
+                if (nextCell) {
+                    nextCell.style.backgroundColor = game.change(game);
+                    game.actionwork(nextCell, game);
+                }
+            }
+            else {
+                col.textContent = num;
+            }
+        }
+    }
+
+    grid = (game) => {
+
+        let table = document.createElement("table");
+        table.id = "tableGrid";
+        table.style.border = "1px solid black";
+        table.style.width = "50%";
+        table.style.margin = "auto";
+
+        // let dataTable;
+        for (let i = 0; i < 10; i++) {
+
+            let row = document.createElement("tr");
+            row.id = "row" + (i + 1);
+
+            for (let j = 0; j < 10; j++) {
+
+                let col = document.createElement("td");
+                col.id = (i + 1) + "col" + "" + (j + 1);
+                col.style.border = "1px solid black";
+                col.style.margin = "auto";
+                col.textContent = "*";
+                col.style.backgroundColor = "white";
+
+                col.addEventListener("click", function () {
+                    console.log("clicked")
+                    let color = col.style.backgroundColor;
+                    let check = false;
+
+                    if (color === "white" || (game.currentPlayer == 1 && color == "red") || (game.currentPlayer == 2 && color == "blue")) {
+                        check = true;
+                    }
+
+                    if (check) {
+                        game.actionwork(col,game);
+                        if (game.currentPlayer == 1) {
+                            game.currentPlayer = 2;
+                            document.getElementById("player").textContent = "Player Two(Blue)";
+                        }
+                        else {
+                            game.currentPlayer = 1;
+                            document.getElementById("player").textContent = "Player One(Red)";
+                        }
+                        game.checkForResult(table,game);
+                    }
+                });
+                row.appendChild(col);
+            }
+            table.appendChild(row);
+
+        }
+        return table;
     }
 }
 
+const createButton = document.getElementById("createButton");
+const createBlock = document.getElementById("gridCreator");
 
 
+console.log(createButton);
+console.log(createBlock);
 
 
+if (createButton != null) {
+    createButton.addEventListener('click', () => {
+        const game = new ChainReaction();
+        const grid = game.grid(game);
 
-
-
+        const table = document.getElementById("grid");
+        table.appendChild(grid);
+        createBlock.style.display = "none";
+        // createBlock.remove();
+    });
+}
